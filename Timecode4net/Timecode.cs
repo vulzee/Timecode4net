8b90650aa282cb0fbb91ddd15e6dd222d215e561
vulzee <vulzee@users.noreply.github.com>
2026-03-19 16:17:56 +0100

    Update Timecode.cs

--- Timecode4net/Timecode.cs
+++ Timecode4net/Timecode.cs
@@ -113,7 +113,7 @@ namespace Timecode4net
 
         public TimeSpan ToTimeSpan()
         {
-            var framesInMsec = this.TotalFrames * FrameRate.msec.ToLong() / this._rawFrameRate.ToDouble();
+            var framesInMsec = this.TotalFrames * FrameRate.msec.ToDouble() / this._rawFrameRate.ToDouble();
             return TimeSpan.FromMilliseconds(framesInMsec);
         }
 
